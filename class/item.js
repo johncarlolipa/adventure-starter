bb9e923bc70708ce97efdf0f9c4308be9f2da2bc
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-04 20:38:22 +0800

    fill out the item class

--- class/item.js
+++ class/item.js
@@ -1,7 +1,13 @@
 class Item {
+  constructor(name, description, value = 0) {
+    this.name = name;
+    this.description = description;
+    this.value = value;
+  }
 
-    // Fill this in
-
+  inspect() {
+    console.log(`You see a ${this.name}. ${this.description}`);
+  }
 }
 
 module.exports = {
