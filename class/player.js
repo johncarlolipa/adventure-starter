715ea77e9e7197db5b9d4f43315e13e733e1926e
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-04 20:44:21 +0800

    fill out the player and room instance

--- class/player.js
+++ class/player.js
@@ -1,56 +1,72 @@
+const { Item } = require('./item');
+
 class Player {
+  constructor(name, startingRoom) {
+    this.name = name;
+    this.currentRoom = startingRoom;
+    this.items = [];
+  }
 
-    constructor(name, startingRoom) {
-        this.name = name;
-        this.currentRoom = startingRoom;
-        this.items = [];
+  move(direction) {
+    const nextRoom = this.currentRoom.getRoomInDirection(direction);
+
+    if (nextRoom) {
+      this.currentRoom = nextRoom;
+      nextRoom.printRoom(this);
+    } else {
+      console.log("You cannot move in that direction");
     }
+  }
 
-    move(direction) {
-
-        const nextRoom = this.currentRoom.getRoomInDirection(direction);
-
-        // If the next room is valid, set the player to be in that room
-        if (nextRoom) {
-            this.currentRoom = nextRoom;
-
-            nextRoom.printRoom(this);
-        } else {
-            console.log("You cannot move in that direction");
-        }
+  printInventory() {
+    if (this.items.length === 0) {
+      console.log(`${this.name} is not carrying anything.`);
+    } else {
+      console.log(`${this.name} is carrying:`);
+      for (let i = 0; i < this.items.length; i++) {
+        console.log(`  ${this.items[i].name}`);
+      }
     }
+  }
 
-    printInventory() {
-        if (this.items.length === 0) {
-            console.log(`${this.name} is not carrying anything.`);
-        } else {
-            console.log(`${this.name} is carrying:`);
-            for (let i = 0 ; i < this.items.length ; i++) {
-                console.log(`  ${this.items[i].name}`);
-            }
-        }
+  takeItem(itemName) {
+    const itemToTake = this.currentRoom.getItemByName(itemName);
+
+    if (itemToTake) {
+      this.items.push(itemToTake);
+      this.currentRoom.items = this.currentRoom.items.filter(item => item !== itemToTake);
+      console.log(`${this.name} picked up ${itemToTake.name}.`);
+    } else {
+      console.log(`${this.name} cannot find ${itemName} in the room.`);
     }
+  }
 
-    takeItem(itemName) {
+  dropItem(itemName) {
+    const itemToDrop = this.getItemByName(itemName);
 
-        // Fill this in
-
+    if (itemToDrop) {
+      this.currentRoom.items.push(itemToDrop);
+      this.items = this.items.filter(item => item !== itemToDrop);
+      console.log(`${this.name} dropped ${itemToDrop.name}.`);
+    } else {
+      console.log(`${this.name} is not carrying ${itemName}.`);
     }
+  }
 
-    dropItem(itemName) {
+  getItemByName(name) {
+    return this.items.find(item => item.name.toLowerCase() === name.toLowerCase());
+  }
 
-        // Fill this in
+  eatItem(itemName) {
+    const foodItem = this.getItemByName(itemName);
+
+    if (foodItem) {
+      console.log(`${this.name} enjoyed eating ${foodItem.name}.`);
+      this.items = this.items.filter(item => item !== foodItem);
+    } else {
+      console.log(`${this.name} does not have ${itemName} to eat.`);
     }
-
-    eatItem(itemName) {
-        // Fill this in
-
-    }
-
-    getItemByName(name) {
-
-        // Fill this in
-    }
+  }
 }
 
 module.exports = {
--- class/room.js
+++ class/room.js
@@ -1,55 +1,51 @@
 class Room {
-
     constructor(name, description) {
-        this.name = name;
-        this.description = description;
-        this.exits = {};
-        this.items = [];
+      this.name = name;
+      this.description = description;
+      this.exits = {};
+      this.items = [];
     }
 
-    printRoom() {
-        console.clear();
-        console.log("");
-        console.log(this.name);
-        console.log("");
-        console.log(this.description);
-        console.log("");
-        if (this.items.length > 0) {
-            console.log(`Items: ${this.items.map(item => item.name).join(", ")}`);
-        }
-        console.log(this.getExitsString());
-        console.log("");
+    printRoom(player) {
+      console.clear();
+      console.log('');
+      console.log(this.name);
+      console.log('');
+      console.log(this.description);
+      console.log('');
+      if (this.items.length > 0) {
+        console.log(`Items: ${this.items.map(item => item.name).join(', ')}`);
+      }
+      console.log(this.getExitsString());
+      console.log('');
+      player.printInventory();
     }
 
     getExits() {
-        return Object.keys(this.exits);
+      return Object.keys(this.exits);
     }
 
     getExitsString() {
-        return `Exits: ${this.getExits().join(", ")}`
+      return `Exits: ${this.getExits().join(', ')}`;
     }
 
     connectRooms(direction, connectingRoom) {
+      if (['n', 's', 'e', 'w'].indexOf(direction) < 0 || !connectingRoom) {
+        throw new Error('Error: Invalid room connection');
+      }
 
-        // Check if the direction and connecting room are valid
-        if (['n', 's', 'e', 'w'].indexOf(direction) < 0 || !connectingRoom) {
-            throw new Error("Error: Invalid room connection");
-        }
-
-        this.exits[direction] = connectingRoom;
+      this.exits[direction] = connectingRoom;
     }
 
     getRoomInDirection(direction) {
-        return this.exits[direction];
+      return this.exits[direction];
     }
 
     getItemByName(name) {
-
-        // Fill this in
+      return this.items.find(item => item.name.toLowerCase() === name.toLowerCase());
     }
+  }
 
-}
-
-module.exports = {
-  Room,
-};
+  module.exports = {
+    Room,
+  };
